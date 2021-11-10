9637244383794a4631dba780ce9454ce3cbac28a
Chandrachapa <vishakabasnayake@gmail.com>
2021-11-10 22:27:47 +0100

    anylogic

---
+++ anylogic/data generation.java
@@ -0,0 +1,91 @@
+System.out.println("4: ");
+	{
+		//physical layer 
+		//Generation of Walsh codes
+		boolean[][] walsh=hadamard(4); 
+		boolean [] code1 =new boolean [4];
+		boolean [] code2 =new boolean [4];
+		boolean [] code3 =new boolean [4];
+        if(thisid==110)
+        	{
+        		code1= walsh[0];//Taking 2nd row of walsh code for User1
+        	}
+        else if(thisid==210)
+        	{
+        		code2 = walsh[2]; //Taking 3rd row of walsh code for User2
+        	}
+        else
+        	{
+        		code3 = walsh[3]; //Taking 3rd row of walsh code for User2
+        	}
+           
+        //data generation
+        int N = 10000;
+        
+        boolean [] userdata =  data_generator(N); //generate data sequence
+        int [] muserdata = new int [N]; //initalize modulated data sequence
+        
+		//modulation
+		for (int i=0; i<userdata.length; i++) 
+			{
+			  if (userdata[i]==true)
+				  {
+				  	muserdata[i] = 2*1 - 1;
+				  }
+			  else
+				  {
+				  	muserdata[i] = - 1;
+				  }
+			}
+		
+		//code and data multiplication 
+		//data_user201*code2
+		double sum = 0; 
+        int [] cuserdata =  new int [N];   
+    	for (int i = 0; i < 4; i++) 
+    		{    if(code1[i]==true)
+	    			{     
+		        	 	cuserdata[i] = muserdata[i] * 1;  
+		        	 }    
+		         else
+			         {
+			         	cuserdata[i] = 0;
+			         }
+	    	}
+	    	
+		color = red;
+		int msgid=(int)uniform(0,main.nbphones+1000);//unique msg id 
+								
+		//emergency call count
+		if(nbrelayers==0 && !alreadyCalled.contains(id)&& main.emergcallids.get(String.valueOf(id)+";"+String.valueOf(msgid))==null&&String.valueOf(id)!=null)
+			{
+				main.emergcallids.put(String.valueOf(id)+";"+String.valueOf(msgid),String.valueOf(id)+" ts:"+time(SECOND));
+			
+				main.EmergCallStatusTime.put(String.valueOf(id),inCoverage); 
+	    		main.EmergCalloriginatedTime.put(String.valueOf(id), String.valueOf(time(SECOND))); //latency	
+	    		main.EmergCallLocx.put(String.valueOf(id),Location_phoneX); 
+	    		main.EmergCallLocy.put(String.valueOf(id), Location_phoneY);
+				
+				if(main.emergcallids.get(String.valueOf(id)+";"+String.valueOf(msgid))==null)//avoiding any null element entering the emerg call ids array
+		  			{
+		  				main.emergcallids.remove(String.valueOf(id)+";"+String.valueOf(msgid));
+		  			}
+		  		else
+		  			{   
+		  				main.totalEmergCalls++;//only if the emerg call ids array is not null emergency call is counted
+		  			}
+			}
+		if(main.emergcallids.get(String.valueOf(id)+";"+String.valueOf(msgid))!=null)//can include retransmissions
+  			{				
+				alreadyCalled.add(id);
+			  	sendToAllConnected("emergency" + ";"+ String.valueOf(id)+";"+String.valueOf(msgid) +";"+ String.valueOf(rssiWatts_max)+";"+String.valueOf(id));//sending 
+			  	y_st = "code";
+			  	for (int i = 0; i < cuserdata.length; i++) 
+		    		{    
+		    			y_st += ";"+String.valueOf(cuserdata[i]);
+			    	}
+			    
+			  	sendToAllConnected(y_st);
+			}								
+		main.energy_source_relay_main += transmitE;	//energy consumption
+	}
---
+++ anylogic/receiver_ber_computation.java
@@ -0,0 +1,154 @@
+//physical layer 
+//noise addition 
+//code multiply with data 
+if (!buffer_tr.isEmpty()){
+receivedmsg=msg;
+nbrelayers=0;
+String[] message = receivedmsg.split(";");
+lastsender=message[1];
+
+int N = 10000;
+int [] rx = new int [N]; 
+if(message[0].equals("code"))
+	{
+	 for (int k = 0; k < message.length; k++) 
+		{    
+			rx[k] = Integer.valueOf(message[k]);
+    	}
+	}
+//add channel effect and noise
+
+double [] channel_real = new double [N];
+
+double [] channel_imag = new double [N];
+
+for (int k = 0; k <message.length; k++)
+	{
+	    channel_real[k]= Math.floor(Math.random() * 5);;   // the real part
+		channel_imag[k] = Math.floor(Math.random() * 5);;   // the imaginary part 	    
+	}   
+	
+double [] rxf_real = new double [N];
+double [] rxf_imag = new double [N];
+//adding channel fading effect on the transmitted signal	
+for (int k = 0; k <rx.length; k++)
+	{
+		rxf_real[k] = channel_real[k]*rx[k];
+		rxf_imag[k] = channel_imag[k]*rx[k];
+	}
+// noise  
+double [] noise_real = new double [N];
+double [] noise_imag = new double [N];
+
+for (int k = 0; k <message.length; k++)
+	{
+	    noise_real[k]= Math.floor(Math.random() * 5);;   // the real part
+		noise_imag[k] = Math.floor(Math.random() * 5);;   // the imaginary part 	    
+	}   
+	
+double [] y_real = new double [N];
+double [] y_imag = new double [N];
+for (int k = 0; k <message.length; k++)
+	{
+		y_real[k] = rxf_real[k]+noise_real[k];
+		y_imag[k] = rxf_imag[k]+noise_imag[k];
+	}
+//decoding phase
+//equalization 
+double [] eq_real = new double [N];
+double [] eq_imag = new double [N];
+double [] eq_abs  = new double [N];
+for (int j = 0; j <message.length; j++)
+	{
+		eq_real[j] = y_real[j]/channel_real[j];
+		eq_imag[j] = y_imag[j]/channel_imag[j];
+		double term1=Math.pow(eq_real[j] ,2);
+		double term2=Math.pow(eq_imag[j] ,2);
+		eq_abs[j]  = Math.pow(term1+term2,0.5);
+	}	
+//checking and estimating the received value as bit 0 or bit 1
+double [] est_bit  = {};
+for (int h = 0; h <message.length; h++)
+	{
+		if (eq_abs[h]>0)
+			{
+				est_bit[h] = 1;
+		    }
+		else
+			{
+				est_bit[h] = 0;
+			}
+	}
+
+//measure total bit error
+int diff =0;
+for (int h = 0; h <message.length; h++)
+	{
+		if(est_bit[h]!=Double.valueOf(message[h]))
+			{
+				diff = 1+diff;
+			}
+	}
+	
+//ber calculation
+double ber = diff/message.length; 	
+
+//if an emergency call is received : energy counter
+if(message[0].equals("emergency")) 	
+	{    
+		congestion_min++;
+		if(!alreadyrelayed.contains("emergency"+";"+ message[1]+";"+message[2]+";"+message[3]))
+			{
+				nbcalls++;
+			}	
+		main.congestion_sum = 1+ main.congestion_sum;
+		if(congestion_min==0)congestion_min = 1;
+		if(nbcalls==0)nbcalls = 1;
+		if(congestion_min>=200)
+		    {
+		    	congestion_high = true;
+		    	n0_th = main.n0_th;
+		    }
+		else if(congestion_min>=10 && congestion_min <200)
+			{
+				congestion_medium = true;
+				n0_th = main.n0_th +1;
+			}
+		else
+			{
+				//congestion_less = true;
+				n0_th = main.n0_th+1;
+			}
+		
+		if(main.rosenbrockTmax==true)rosenbrock();
+		if(Tmax<0)Tmax = 0;
+		else if(Tmax>=120)Tmax = 120;
+		waitingTime=min(Tmax,1*(1-ic)*(1/(locAccuracy+0.1))*(Double.valueOf(message[3])/rssiWatts_max)*(100/min(energyLevel,100)));
+		
+		main.energy_source_relay_main += receiveE;	
+		buffer_tr.put(message[1],time(SECOND)+waitingTime);	
+		buffer_rssi.put(message[1],message[3]);
+		buffer_msgid.put(message[1],message[2]);
+		buffer_relayid.put(message[1],message[4]);
+		
+		//System.out.println("rssi ratio:" +(Double.valueOf(message[3])/rssiWatts_max));
+		main.rssi_ratio.add(Double.valueOf(message[3])/rssiWatts_max);	
+	}
+	
+if (main.EmergCallReachedgNBTime.containsKey(message[1]) && inCoverage == true)
+	{
+		buffer_tr.remove(message[1]);
+		alreadyrelayed.add("emergency"+";"+ message[1]+";"+message[2] +";"+message[3]);		
+	}	
+	
+String[] arrayKeys = buffer_nbrelayers.keySet().toArray( new String[buffer_nbrelayers.size()]);		
+for(int k=0;k<buffer_nbrelayers.size();k++)
+	{	
+		if(buffer_nbrelayers.get(arrayKeys[k])>=(main.max_rs_th-buffer_tr.size()))
+			{
+				buffer_tr.remove(arrayKeys[k]);
+				buffer_nbrelayers.remove(arrayKeys[k]);	
+				alreadyrelayed.add("emergency"+";"+ arrayKeys[k]+";"+buffer_msgid.get(arrayKeys[k]) +";"+buffer_rssi.get(arrayKeys[k]));	
+			}
+	}
+	}
